10a2ad2ccdd39c06ce4194a7aaa26fc2b8116032
VladH <vlad.haprian@epfl.ch>
2014-06-07 18:19:37 +0300

    All error fixed

--- components/example/src/Integrals/ComplexIntegral.scala
+++ components/example/src/Integrals/ComplexIntegral.scala
@@ -6,17 +6,22 @@ import Algebra.Korp
 import spire.algebra._
 import Algebra.Korp
 
-class ComplexIntegral[T: Korp /* not <: Korp[T]!!! */ ]{
+class ComplexIntegral[T: Korp]{
 
   private type complexMethod = (T => T ,T, T) => T
 
+  //> leftRect: (f: Double => Double, a: Double, b: Double)Double
   private def leftRect(f:T => T, a:T, b:T) = f(a)
-                                                  //> leftRect: (f: Double => Double, a: Double, b: Double)Double
-  // NOTE: You can't divide by 2, since 2 is not part of your group
-  private def midRect(f:T => T, a:T, b:T) = f((a + b)/2)
-                                                  //> midRect: (f: Double => Double, a: Double, b: Double)Double
+  
+  
+  // NOTE: We can't divide by 2, since 2 is not part of the group
+  //> midRect: (f: Double => Double, a: Double, b: Double)Double
+  //private def midRect(f:T => T, a:T, b:T) = f((a + b)/2)
+  
+  //> rightRect: (f: Double => Double, a: Double, b: Double)Double
   private def rightRect(f:T => T, a:T, b:T) = f(b)
-                                                  //> rightRect: (f: Double => Double, a: Double, b: Double)Double
+ 
+  
 
   private def integrateComplex(f: T => Complex[T], a: T, b: T, steps: T, zero: T, m:complexMethod): Complex[T]= {
     val delta = (b-a)/steps
@@ -38,7 +43,7 @@ class ComplexIntegral[T: Korp /* not <: Korp[T]!!! */ ]{
 
   def computeAndPrint(f:T=>Complex[T], a:T, b:T, steps:T, zero:T)={
     println("rectangular left :"+integrateComplex(f, a, b, steps,zero, leftRect))
-    println("rectangular middle :"+integrateComplex(f, a, b, steps,zero, midRect))
+   // println("rectangular middle :"+integrateComplex(f, a, b, steps,zero, midRect))
     println("rectangular right :"+integrateComplex(f, a, b, steps,zero, rightRect))
 
   }
--- components/example/src/Integrals/Test.scala
+++ components/example/src/Integrals/Test.scala
@@ -7,9 +7,23 @@ object Test {
       def fn1(x:Double)=x*x*x
       def fn2(x:Double)=1/x
       def complexFn(x:Double)= Complex(fn1(x),fn2(x))
-
-// TODO: Define Korp[Double] with its operations!
-// val integral = new ComplexIntegral[Double]()
-// integral.computeAndPrint(complexFn, 2, 5,0, 100)
+      
+      //Defining Korp[Double] with its operations
+      implicit object myKorp extends Korp[Double]{
+    	  	def zero = 0
+    	  	def one = 1 
+    	  	def plus(x:Double,y:Double):Double = x+y
+    	  	def negate(x:Double):Double = -x
+    	  	def div(x:Double,y:Double):Double = x/y
+    	  	def times(x:Double,y:Double):Double = x*y
+    	  	def compare(x:Double,y:Double):Int = 
+    	  	  if(x<y) -1 
+    	  	  else if (x > y) 1
+    	  	  else 0
+    	  	
+      }
+      
+      val integral = new ComplexIntegral[Double]()
+      integral.computeAndPrint(complexFn, 2, 5,0, 100)
   }
 }
