3bd2c16178f5ba3aefae67d36c4b830d68fb856c
VladH <vlad.haprian@epfl.ch>
2014-06-27 21:00:00 +0200

    Benchmark is ok and we have results

--- project/Build.scala
+++ project/Build.scala
@@ -46,7 +46,9 @@ object MiniboxingBuild extends Build {
       "com.google.guava" % "guava" % "r09",
       "com.google.code.java-allocation-instrumenter" % "java-allocation-instrumenter" % "2.0",
       "com.google.code.caliper" % "caliper" % "1.0-SNAPSHOT" from "http://plastic-idolatry.com/jars/caliper-1.0-SNAPSHOT.jar",
-      "com.google.code.gson" % "gson" % "1.7.1")
+      "com.google.code.gson" % "gson" % "1.7.1"),
+      // enable forking in run
+      fork in run := true
   )
 
   val scalaMeter = {
