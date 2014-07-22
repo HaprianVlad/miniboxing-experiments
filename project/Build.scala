932e3112e900c6ded43328e53291a42e3dcdd66a
VladH <vlad.haprian@epfl.ch>
2014-07-22 12:55:40 +0200

    Updated miniboxed version, still the Null pointer exception

--- project/Build.scala
+++ project/Build.scala
@@ -71,14 +71,14 @@ object MiniboxingBuild extends Build {
 /** Settings for the miniboxing plugin */
   lazy val miniboxingSettings = Seq[Setting[_]](
     resolvers += Resolver.sonatypeRepo("snapshots"),
-    libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.3-SNAPSHOT",
-    addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.3-SNAPSHOT"),
+    libraryDependencies += "org.scala-miniboxing.plugins" %% "miniboxing-runtime" % "0.4-SNAPSHOT",
+    addCompilerPlugin("org.scala-miniboxing.plugins" %% "miniboxing-plugin" % "0.4-SNAPSHOT"),
     scalacOptions ++= (
      // "-P:minibox:log" ::    // enable the miniboxing plugin output
       //                       // (which explains what the plugin is doing)
-    //  "-P:minibox:hijack" :: // enable hijacking the @specialized annotations
+     "-P:minibox:hijack" :: // enable hijacking the @specialized annotations
       //                       // transforming them into @miniboxed annotations
-      "-optimize" ::         // necessary to get the best performance when
+     "-optimize" ::         // necessary to get the best performance when
                              // using the miniboxing plugin
       Nil
     )
